36acfa81a8866abb7f62eaf068c8bf8999ea81cf
Суворов Николай Михайлович <nmsuvorov@edu.hse.ru>
2018-06-12 16:49:18 +0500

    upd

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -8,38 +8,41 @@ namespace Task11
 {
     class Program
     {
-        static string Code(string input)
+        static string Code(string input)//Создание зашифрованного сообщения
         {
             string result = "";
-            result += input[0];
+            result += input[0];//Первый элемент не изменяется
             for (int i = 1; i < input.Length; i++)
             {
+                //Если текущий элемент (input[i]) равен предыдущему (input[i - 1]) в изначальной строке
+                //То к зашифрованному сообщению добавляется "1", иначе "0"
                 if (input[i] == input[i - 1])
                     result += "1";
                 else result += "0";
             }
             return result;
         }
-        static string Decode(string code)
+        static string Decode(string code)//Создание расшифрованного сообщения
         {
             string result = "";
+            //Первый элемент не изменяется
             result += code[0];
-            char temp = code[0];
             for (int i = 1; i < code.Length; i++)
             {
+                //Если в коде "1", значит элемент не менялся, следовательно дописываем последний известный элемент
                 if (code[i] == '1')
-                    result += temp;
+                    result += result[i - 1];
                 else
                 {
-                    if (temp == '0')
+                    //Если в коде "0", значит элемент на этой позиции изменился и он не равен последнему известному элементу
+                    //Соответственно, к итоговой строке добавляем противоположный temp символ и меняем на него переменную temp
+                    if (result[i-1] == '0')
                     {
-                        result += "1";
-                        temp = '1';
+                        result += "1";                       
                     }
                     else
                     {
-                        result += "0";
-                        temp = '0';
+                        result += "0";                        
                     }
                 }
             }
@@ -47,8 +50,21 @@ namespace Task11
         }
         static void Main(string[] args)
         {
-            string t = "11111111111000000101010101010101010101001";
-            string temp = Code(t);
+            string tocheck;
+            string str;
+            //Проверка на то, что введенная строка состоит только из "0" и "1"
+            do
+            {
+                Console.WriteLine("Введите сообщение для его шифрования и расшифровки: ");
+                str = Console.ReadLine();
+                tocheck = str.Replace("1", "");
+                tocheck = tocheck.Replace("0", "");
+                if (tocheck != "") Console.WriteLine("Введена некорректная строка!");
+            } while (tocheck != "" || str == "");
+            Console.WriteLine("Закодированное сообщение: ");
+            string temp = Code(str);
+            Console.WriteLine(temp);
+            Console.WriteLine("Расшифрованное сообщение: ");
             Console.WriteLine(Decode(temp));
             Console.Read();
         }
